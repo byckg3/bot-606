f05982f7a3f2f32df519e3e2439dbc10f2feb601
byc <byckg3@gmail.com>
2021-03-27 21:01:00 +0800

    add link for readme

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # bot-606
 - Link
     - [Discord Developer Portal](https://discord.com/developers/applications)
+    - [Heroku Dashboard](https://dashboard.heroku.com/)
 - Reference
     - [Making and Deploying Discord Bot with Python](https://dev.to/p014ri5/making-and-deploying-discord-bot-with-python-4hep)
--- src/bot/main.py
+++ src/bot/main.py
@@ -6,15 +6,15 @@ token = os.getenv( "DISCORD_BOT_TOKEN" )
 
 @client.event
 async def on_ready():
-    print( 'We have logged in as {0.user}'.format( client ) )
+    print( "We have logged in as {0.user}".format( client ) )
 
 @client.event
 async def on_message( message ):
     if message.author == client.user:
         return
 
-    if message.content.startswith( '$hello' ):
-        await message.channel.send( 'Fuck u!!' )
+    if message.content.startswith( "$hello" ):
+        await message.channel.send( "Fuck u!!" )
 
 print( "bot is working..." )
 client.run( token )
